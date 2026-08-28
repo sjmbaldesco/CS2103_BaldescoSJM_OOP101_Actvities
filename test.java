9c39cc6569054e8db6a0abbd196b38d323750cea
sjmbaldesco <sirjairus@macabuhay.asia>
2026-08-28 08:53:52 +0800

    Initial commit

---
+++ test.java
@@ -0,0 +1,6 @@
+class test{
+    public static void main(String[]args){
+        System.out.println("Hello Everyone!");
+        System.out.println("This is OOP 101: Object-oriented Programming!");
+    }
+}
